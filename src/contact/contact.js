f1c3655ea8a599cc7114be0a901d7b45352f59b9
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-10-10 19:34:43 +0330

    create desktop

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,6 +1,7 @@
 //load styles
 import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
 import '../init.scss' ;
+import '../effects.scss' ;
 import '../index/styles/header.scss' ;
 import '../contact/styles/contact.scss' ;
 import '../index/styles/footer.scss' ;
