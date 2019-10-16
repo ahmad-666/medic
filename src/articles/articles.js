4bcee8b82234e5a744f83fba19c6e022367406c2
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-10-16 11:16:48 -0700

    before resp v1

---
+++ src/articles/articles.js
@@ -0,0 +1,9 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../framework.scss' ;
+import '../init.scss' ;
+import '../index/styles/header.scss' ;
+import '../articles/styles/articles.scss' ;
+import '../index/styles/footer.scss' ;
+//load scripts
+//load videos 
